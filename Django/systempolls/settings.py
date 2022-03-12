e3cbc34566c160df8360033517f3360dd7b91de3
ValentinaRippe <lauravalentina200102@gmail.com>
2022-03-11 23:22:03 -0500

    make it better

--- Django/systempolls/settings.py
+++ Django/systempolls/settings.py
@@ -9,7 +9,19 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
+import dj_database_url  
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env)
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (  
+    os.path.join(BASE_DIR, 'static'),
+)
+# Update database configuration with $DATABASE_URL.
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
